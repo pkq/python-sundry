162efd53ccf05e155d9692c7eabb15b2392b4520
Patrick Kennedy <pkq@users.noreply.github.com>
2021-04-26 11:10:59 -0700

    Add a second function to python_sundry.py

--- python_sundry.py
+++ python_sundry.py
@@ -2,3 +2,12 @@ def remove_list_item(*, list_name, item_name):
 
   new_list = [ item for item in list_name if item != item_name]
   return new_list
+
+def plot_x_by_class_y(*, table, x_column, y_column):
+  assert isinstance(table, pd.core.frame.DataFrame), f'table is not a dataframe but instead a {type(table)}'
+  assert x_column in table.columns, f'unrecognized column: {x_column}. Check spelling and case.'
+  assert y_column in table.columns, f'unrecognized column: {y_column}. Check spelling and case.'
+  assert table[y_column].nunique()<=5, f'y_column must be of 5 categories or less but has {table[y_column].nunique()}'
+
+  pd.crosstab(table[x_column], table[y_column]).plot(kind='bar', figsize=(15,8), grid=True, logy=True)
+  return None
